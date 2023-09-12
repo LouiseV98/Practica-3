e609c5d745caa2bc17d3368d066fe26528dcb2de
LouiseV98 <143037181+LouiseV98@users.noreply.github.com>
2023-09-11 21:59:36 -0600

    Actualizacion RR
    Ahora toma el primer valor como tiempo de ejecucion

--- Practica 3.py
+++ Practica 3.py
@@ -1,6 +1,7 @@
 import time
 
 def fifo(procesos):
+    print("FIFO\n")
     tiempo_actual = 0
     cola = list(procesos)
     resultado = []
@@ -15,7 +16,7 @@ def fifo(procesos):
         
         tiempo_actual = tiempo_inicio + tiempo_cpu
 
-        #time.sleep(2)
+        time.sleep(2)
         
         print(f"Proceso: {proceso_actual[0]}, Inicio: {tiempo_inicio}, Finalización: {tiempo_inicio + tiempo_cpu}")
 
@@ -23,6 +24,7 @@ def fifo(procesos):
 
 
 def sjf(procesos):
+    print("SJF\n")
     tiempo_total = 0
     tiempo_acumulado = 1
 
@@ -49,56 +51,65 @@ def sjf(procesos):
         if tiempo_cpu > 0:
             procesos.append((nombre, tiempo_llegada, tiempo_cpu))
 
+        time.sleep(2)
+
         print(f"Proceso: {proceso_actual[0]}, Inicio: {tiempo_inicio}, Finalización: {tiempo_inicio + tiempo_ejecucion}")
 
 def round_robin(procesos, quantum):
     tiempo_actual = 0
     cola = list(procesos)
     resultado = []
-    
     while cola:
         proceso_actual = cola.pop(0)
-        tiempo_inicio = max(proceso_actual[1], tiempo_actual)
-        tiempo_cpu = min(quantum, proceso_actual[2])
-        
+        tiempo_inicio = max(tiempo_actual, 0 if tiempo_actual == 0 else tiempo_actual)
+        tiempo_cpu = min(quantum, proceso_actual[1])
+
         resultado.append((proceso_actual[0], tiempo_inicio, tiempo_inicio + tiempo_cpu))
-        
-        proceso_actual = (proceso_actual[0], proceso_actual[1], proceso_actual[2] - tiempo_cpu)
-        
+        proceso_actual = (proceso_actual[0], proceso_actual[1] - tiempo_cpu)
+
         tiempo_actual = tiempo_inicio + tiempo_cpu
 
         #time.sleep(2)
-        
+
         print(f"Proceso: {proceso_actual[0]}, Inicio: {tiempo_inicio}, Finalización: {tiempo_inicio + tiempo_cpu}")
-        
-        # Si hay más tiempo en el quantum y tiempo restante en el proceso, el proceso vuelve a entrar
-        if proceso_actual[2] > 0:
+
+        if proceso_actual[1] > 0:
             cola.append(proceso_actual)
-    
+
     return resultado
 
 def cargar_procesos_desde_archivo(nombre_archivo):
     procesos = []
     with open(nombre_archivo, "r") as archivo:
         for linea in archivo:
-            nombre, tiempo_llegada, tiempo_cpu = linea.strip().split(",")
-            procesos.append((nombre, int(tiempo_llegada), int(tiempo_cpu)))
+            nombre, tiempo_ejecucion, prioridad = linea.strip().split(",")
+            procesos.append((nombre, int(tiempo_ejecucion), int(prioridad)))
     # Ordenar procesos por tiempo de llegada de menor a mayor
     procesos = sorted(procesos, key=lambda x: x[1])
+    return procesos
+
+def cargar_procesos_desde_archivo2(nombre_archivo):
+    procesos = []
+    with open(nombre_archivo, "r") as archivo:
+        for linea in archivo:
+            nombre, tiempo_ejecucion, prioridad = linea.strip().split(",")
+            procesos.append((nombre, int(tiempo_ejecucion), int(prioridad)))
+    # Ordenar procesos por tiempo de llegada de menor a mayor
+    #procesos = sorted(procesos, key=lambda x: x[1])
     return procesos
 
 if __name__ == "__main__":
     archivo_procesos = "procesos.txt"  # Nombre del archivo de procesos
     quantum = 3
     
-    procesos = cargar_procesos_desde_archivo(archivo_procesos)
-    procesos2 = cargar_procesos_desde_archivo(archivo_procesos)
-    proceso3 = cargar_procesos_desde_archivo(archivo_procesos)
-    resultado2 = sjf(procesos2)
+    procesos = cargar_procesos_desde_archivo2(archivo_procesos)
+    #procesos2 = cargar_procesos_desde_archivo(archivo_procesos)
+    #proceso3 = cargar_procesos_desde_archivo(archivo_procesos)
+    #resultado2 = sjf(procesos2)
     
     #print("Simulación de Round Robin:")
     #print("Proceso\tInicio\tFinalización")
     
     resultado = round_robin(procesos, quantum)
-    resultado3 = fifo(proceso3)
+    #resultado3 = fifo(proceso3)
 
