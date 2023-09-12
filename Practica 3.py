9763dbddd352f45c8e994977d4ff61e76ac16476
LouiseV98 <143037181+LouiseV98@users.noreply.github.com>
2023-09-11 18:08:27 -0600

    3 algoritmos
    Corregi RR, agregue SFJ y FIFO

--- Practica 3.py
+++ Practica 3.py
@@ -1,5 +1,56 @@
 import time
 
+def fifo(procesos):
+    tiempo_actual = 0
+    cola = list(procesos)
+    resultado = []
+    
+    while cola:
+        proceso_actual = cola.pop(0)
+        tiempo_inicio = max(proceso_actual[1], tiempo_actual)
+        tiempo_cpu = proceso_actual[2]
+        resultado.append((proceso_actual[0], tiempo_inicio, tiempo_inicio + tiempo_cpu))
+        
+        proceso_actual = (proceso_actual[0], proceso_actual[1], proceso_actual[2] - tiempo_cpu)
+        
+        tiempo_actual = tiempo_inicio + tiempo_cpu
+
+        #time.sleep(2)
+        
+        print(f"Proceso: {proceso_actual[0]}, Inicio: {tiempo_inicio}, Finalización: {tiempo_inicio + tiempo_cpu}")
+
+    return resultado
+
+
+def sjf(procesos):
+    tiempo_total = 0
+    tiempo_acumulado = 1
+
+    while procesos:
+        # Ordenar los procesos por tiempo de CPU más corto
+        procesos.sort(key=lambda x: x[2])
+
+        proceso_actual = procesos.pop(0)
+        nombre, tiempo_llegada, tiempo_cpu = proceso_actual
+        tiempo_inicio = tiempo_acumulado
+        tiempo_ejecucion = tiempo_cpu
+        tiempo_acumulado = tiempo_inicio + tiempo_ejecucion
+        #print(f"Ejecutando {nombre} durante {tiempo_cpu} unidades de tiempo")
+
+        tiempo_cpu -= tiempo_ejecucion
+        #tiempo_total += tiempo_cpu
+
+        # Reducir el tiempo de CPU del proceso actual
+        #tiempo_cpu = 0
+
+        # Verificar si hay otros procesos que llegaron durante la ejecución
+        procesos = [p for p in procesos if p[2] > tiempo_total]
+
+        if tiempo_cpu > 0:
+            procesos.append((nombre, tiempo_llegada, tiempo_cpu))
+
+        print(f"Proceso: {proceso_actual[0]}, Inicio: {tiempo_inicio}, Finalización: {tiempo_inicio + tiempo_ejecucion}")
+
 def round_robin(procesos, quantum):
     tiempo_actual = 0
     cola = list(procesos)
@@ -8,17 +59,17 @@ def round_robin(procesos, quantum):
     while cola:
         proceso_actual = cola.pop(0)
         tiempo_inicio = max(proceso_actual[1], tiempo_actual)
-        tiempo_ejecucion = min(quantum, proceso_actual[2])
+        tiempo_cpu = min(quantum, proceso_actual[2])
         
-        resultado.append((proceso_actual[0], tiempo_inicio, tiempo_inicio + tiempo_ejecucion))
+        resultado.append((proceso_actual[0], tiempo_inicio, tiempo_inicio + tiempo_cpu))
         
-        proceso_actual = (proceso_actual[0], proceso_actual[1], proceso_actual[2] - tiempo_ejecucion)
+        proceso_actual = (proceso_actual[0], proceso_actual[1], proceso_actual[2] - tiempo_cpu)
         
-        tiempo_actual = tiempo_inicio + tiempo_ejecucion
+        tiempo_actual = tiempo_inicio + tiempo_cpu
 
-        time.sleep(2)
+        #time.sleep(2)
         
-        print(f"Proceso: {proceso_actual[0]}, Inicio: {tiempo_inicio}, Finalización: {tiempo_inicio + tiempo_ejecucion}")
+        print(f"Proceso: {proceso_actual[0]}, Inicio: {tiempo_inicio}, Finalización: {tiempo_inicio + tiempo_cpu}")
         
         # Si hay más tiempo en el quantum y tiempo restante en el proceso, el proceso vuelve a entrar
         if proceso_actual[2] > 0:
@@ -30,8 +81,8 @@ def cargar_procesos_desde_archivo(nombre_archivo):
     procesos = []
     with open(nombre_archivo, "r") as archivo:
         for linea in archivo:
-            nombre, tiempo_llegada, tiempo_ejecucion = linea.strip().split(",")
-            procesos.append((nombre, int(tiempo_llegada), int(tiempo_ejecucion)))
+            nombre, tiempo_llegada, tiempo_cpu = linea.strip().split(",")
+            procesos.append((nombre, int(tiempo_llegada), int(tiempo_cpu)))
     # Ordenar procesos por tiempo de llegada de menor a mayor
     procesos = sorted(procesos, key=lambda x: x[1])
     return procesos
@@ -41,9 +92,13 @@ if __name__ == "__main__":
     quantum = 3
     
     procesos = cargar_procesos_desde_archivo(archivo_procesos)
+    procesos2 = cargar_procesos_desde_archivo(archivo_procesos)
+    proceso3 = cargar_procesos_desde_archivo(archivo_procesos)
+    resultado2 = sjf(procesos2)
     
-    print("Simulación de Round Robin:")
-    print("Proceso\tInicio\tFinalización")
+    #print("Simulación de Round Robin:")
+    #print("Proceso\tInicio\tFinalización")
     
     resultado = round_robin(procesos, quantum)
+    resultado3 = fifo(proceso3)
 
